b1837b2036b8fb8d80311d8fa317959e721848dc
Patent2net <patent2net@gmail.com>
2016-11-01 18:13:39 +0100

    Respirator case

---
+++ DATA/Respirator/Respirator.html_files/RespiratorFP.mm
@@ -0,0 +1,252 @@
+<map version="freeplane 1.3.0"> 
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net --> 
+<node TEXT="Project: Respirator " BACKGROUND_COLOR="#FFFF33"> 
+<hook NAME="MapStyle"> 
+   <properties show_note_icons="true"/>
+</hook> 
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/> 
+<richcontent TYPE="NOTE"> 
+<html> 
+  <head> 
+  </head> 
+  <body> 
+    <p> 
+      Chave de pesquisa: TI=Respirator AND IPC= A61M1 
+    </p> 
+    <p> 
+      Total de patentes encontradas: 23 
+    </p> 
+  </body> 
+</html> 
+</richcontent> 
+<node TEXT="A: Human necessities (23 Patents)" POSITION="left"  BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="200"> 
+<edge COLOR="#0000ff"/> 
+<font NAME="Serif" SIZE="16"/> 
+<node TEXT="A61	Medical or veterinary science; Hygiene
+ (23 Patents)" POSITION="left"  BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="12"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="A61M	Devices for introducing media into, or onto, the body ; Devices for producing or ending sleep or stupor
+ (22 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="12"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="A61M16	Devices for influencing the respiratory system of patients by gas treatment, e.g. mouth-to-mouth respiration; Tracheal tubes
+ (16 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="A61M16/22	Carbon dioxide-absorbing devices
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61M16/04	Tracheal tubes
+ (3 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61M16/20	Valves specially adapted to medical respiratory devices
+ (3 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61M16/12	by mixing different gases
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61M16/06	Respiratory or anaesthetic masks
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61M16/08	Bellows; Connecting tubes
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+</node> 
+<node TEXT="A61M1	Suction or pumping devices for medical purposes; Devices for carrying-off, for treatment of, or for carrying-over, body-liquids; Drainage systems
+ (22 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="A61M16/22	Carbon dioxide-absorbing devices
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61M16/04	Tracheal tubes
+ (3 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61M16/20	Valves specially adapted to medical respiratory devices
+ (3 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61M16/12	by mixing different gases
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61M16/06	Respiratory or anaesthetic masks
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61M16/08	Bellows; Connecting tubes
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61M1/14	Dialysis systems; Artificial kidneys; Blood oxygenators (semi-permeable membranes characterised by the material, manufacturing processes therefor B01D0071000000)
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61M1/18	in the shape of hollow fibres
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+</node> 
+<node TEXT="A61M25	Catheters; Hollow probes
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="A61M25/01	Introducing, guiding, advancing, emplacing or holding catheters (A61M0025100000 takes precedence);;
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+</node> 
+<node TEXT="A61M3	Medical syringes, e.g. enemata; Irrigators
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61M5	Devices for bringing media into the body in a subcutaneous, intra-vascular or intramuscular way; Accessories therefor, e.g. filling or cleaning devices, arm rests
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+</node> 
+<node TEXT="A61B	Diagnosis; Surgery; Identification 
+ (3 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="8"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="A61B5	Measuring for diagnostic purposes ; Identification of persons
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="A61B5/02	Measuring pulse, heart rate, blood pressure or blood flow; Combined pulse/heart-rate/blood pressure determination; Evaluating a cardiovascular condition not otherwise provided for, e.g. using combinations of techniques provided for in this group with electrocardiography; Heart catheters for measuring blood pressure
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+</node> 
+<node TEXT="A61B17	Surgery
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="A61B17/24	for use in the oral cavity, larynx, bronchial passages or nose (for medical inspection of cavities or tubes in the body A61B0001000000); Tongue scrapers
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+<node TEXT="A61B17/50	Instruments, other than pincettes or toothpicks, for removing foreign bodies from the human body
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+</node> 
+<node TEXT="A61B10	Other methods or instruments for diagnosis, e.g. for vaccination diagnosis; Sex determination; Ovulation-period determination; Throat striking implements
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+</node> 
+<node TEXT="A61H	Physical therapy apparatus, e.g. Devices for locating or stimulating reflex points in the body; Artificial respiration; Massage; Bathing devices for special therapeutic or hygienic purposes or specific parts of the body 
+ (2 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="8"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="A61H31	Artificial respiration or heart stimulation, e.g. heart massage 
+ (2 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="A61H31/02	Iron-lungs, whether or not combined with gas breathing means
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#CCFFFF" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+</node> 
+</node> 
+</node> 
+</node> 
+<node TEXT="F: Mechanical engineering; Lighting; Heating; Weapons; Blasting (1 Patents)" POSITION="right"  BACKGROUND_COLOR="#CCFF99" STYLE="bubble" MAX_WIDTH="200"> 
+<edge COLOR="#0000ff"/> 
+<font NAME="Serif" SIZE="8"/> 
+<node TEXT="F16	Engineering elements or units; General measures for producing and maintaining effective functioning of machines or installations; Thermal insulation in general
+ (1 Patents)" POSITION="right"  BACKGROUND_COLOR="#CCFF99" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="12"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="F16K	Valves; Taps; Cocks; Actuating-floats; Devices for venting or aerating
+ (1 Patents)" POSITION="right" BACKGROUND_COLOR="#CCFF99" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="12"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="F16K5	Taps or cocks comprising only cut-off apparatus having at least one of the sealing faces shaped as a more or less complete surface of a solid of revolution, the opening and closing movement being predominantly rotary
+ (1 Patents)" POSITION="right" BACKGROUND_COLOR="#CCFF99" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="F16K5/02	with plugs having conical surfaces; Packings therefor
+ (1 Patents)" POSITION="right" BACKGROUND_COLOR="#CCFF99" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+</node> 
+</node> 
+</node> 
+</node> 
+<node TEXT="B: Performing operations; Trasnsporting (1 Patents)" POSITION="left"  BACKGROUND_COLOR="#FFFF99" STYLE="bubble" MAX_WIDTH="200"> 
+<edge COLOR="#0000ff"/> 
+<font NAME="Serif" SIZE="8"/> 
+<node TEXT="B01	Physical or chemical processes or apparatus in general
+ (1 Patents)" POSITION="left"  BACKGROUND_COLOR="#FFFF99" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="12"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="B01D	Separation 
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#FFFF99" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="12"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="B01D69	Semi-permeable membranes for separation processes or apparatus characterised by their form, structure or properties; Manufacturing processes specially adapted therefor
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#FFFF99" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="B01D69/08	Hollow fibre membranes (manufacture of hollow fibres D01D0005240000, D01F0001080000)
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#FFFF99" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+</node> 
+<node TEXT="B01D71	Semi-permeable membranes for separation processes or apparatus characterised by the material; Manufacturing processes specially adapted therefor
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#FFFF99" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+<node TEXT="B01D71/38	Polyalkenylalcohols; Polyalkenylesters; Polyalkenylethers; Polyalkenylaldehydes; Polyalkenylketones; Polyalkenylacetals; Polyalkenylketals
+ (1 Patents)" POSITION="left" BACKGROUND_COLOR="#FFFF99" STYLE="bubble" MAX_WIDTH="300">
+<font SIZE="10"/> 
+<edge COLOR="#0000ff"/> 
+</node> 
+</node> 
+</node> 
+</node> 
+</node> 
+</node> 
+</map> 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
