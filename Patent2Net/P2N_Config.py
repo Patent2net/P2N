20d9db6497f4e53320fef8277101c76ab6ea6289
Roberto Faga <faga@caule.net>
2017-03-25 22:53:43 -0300

    Adding Family networks feature

--- Patent2Net/P2N_Config.py
+++ Patent2Net/P2N_Config.py
@@ -4,27 +4,79 @@ import sys
 class P2NConfig:
     def __init__(self):
 
+        # Initiate all empty attributes
+        self.requete = ""
+        self.ndf = ""
+        self.GatherContent = False
+        self.GatherBiblio = False
+        self.GatherPatent = False
+        self.GatherFamilly = False
+        self.GatherIramuteq = False
+        # Networks config loading
+        self.InventorNetwork = False
+        self.ApplicantNetwork = False
+        self.ApplicantInventorNetwork = False
+        self.InventorCrossTechNetwork = False
+        self.ApplicantCrossTechNetwork = False
+        self.CountryCrossTechNetwork = False
+        self.CrossTechNetwork = False
+        self.CompleteNetwork = False
+        self.FamiliesNetwork = False
+        self.FamiliesHierarchicNetwork = False
+        self.References = False
+        self.Citations = False
+        self.Equivalents = False
+
         #opening request file, reading parameters
-        if len(sys.argv) > 1:
-            content = open(sys.argv[1], "r").readlines()
-        else:
-            content = open("..//requete.cql", "r").readlines()
+        # if len(sys.argv) > 1:
+        #     content = open(sys.argv[1], "r").readlines()
+        # else:
+        content = open("..//requete.cql", "r").readlines()
 
         for line in content:
-            if line.count('request:')>0:
+            ### General config loading
+            if line.count('request:') > 0:
                 self.requete = self.getStr(line)
-            if line.count('DataDirectory:')>0:
+            elif line.count('DataDirectory:') > 0:
                 self.ndf = self.getStr(line)
-            if line.count('GatherContent')>0:
+            elif line.count('GatherContent') > 0:
                 self.GatherContent = self.getBoolean(line)
-            if line.count('GatherBiblio')>0:
+            elif line.count('GatherBiblio') > 0:
                 self.GatherBiblio = self.getBoolean(line)
-            if line.count('GatherPatent')>0:
+            elif line.count('GatherPatent') > 0:
                 self.GatherPatent = self.getBoolean(line)
-            if line.count('GatherFamilly')>0:
+            elif line.count('GatherFamilly') > 0:
                 self.GatherFamilly = self.getBoolean(line)
-            if line.count('OPSGatherContentsv2-Iramuteq')>0:
+            elif line.count('OPSGatherContentsv2-Iramuteq') > 0:
                 self.GatherIramuteq = self.getBoolean(line)
+
+            # Networks config loading
+            elif line.count('InventorNetwork') > 0:
+                self.InventorNetwork = self.getBoolean(line)
+            elif line.count('ApplicantNetwork') > 0:
+                self.ApplicantNetwork = self.getBoolean(line)
+            elif line.count('ApplicantInventorNetwork') > 0:
+                self.ApplicantInventorNetwork = self.getBoolean(line)
+            elif line.count('InventorCrossTechNetwork') > 0:
+                self.InventorCrossTechNetwork = self.getBoolean(line)
+            elif line.count('ApplicantCrossTechNetwork') > 0:
+                self.ApplicantCrossTechNetwork = self.getBoolean(line)
+            elif line.count('CountryCrossTechNetwork') > 0:
+                self.CountryCrossTechNetwork = self.getBoolean(line)
+            elif line.count('CrossTechNetwork') > 0:
+                self.CrossTechNetwork = self.getBoolean(line)
+            elif line.count('CompleteNetwork') > 0:
+                self.CompleteNetwork = self.getBoolean(line)
+            elif line.count('FamiliesNetwork') > 0:
+                self.FamiliesNetwork = self.getBoolean(line)
+            elif line.count('FamiliesHierarchicNetwork') > 0:
+                self.FamiliesHierarchicNetwork = self.getBoolean(line)
+            elif line.count('References') > 0:
+                self.References = self.getBoolean(line)
+            elif line.count('Citations') > 0:
+                self.Citations = self.getBoolean(line)
+            elif line.count('Equivalents') > 0:
+                self.Equivalents = self.getBoolean(line)
 
         self.generatePaths()
 
@@ -34,13 +86,15 @@ class P2NConfig:
         self.ResultContents = '..//DATA//'+self.ndf+'//PatentContents'
         self.temporPath = '..//DATA//'+self.ndf+'//tempo'
         self.ResultAbstractPath = self.ResultContents+'//Abstract'
+        self.ResultPathGephi = '..//DATA//' + self.ndf + '//GephiFiles'
 
         for path in [
             self.ListPatentPath,
             self.ResultPathBiblio,
             self.ResultContents,
             self.temporPath,
-            self.ResultAbstractPath
+            self.ResultAbstractPath,
+            self.ResultPathGephi,
         ]:
             if not os.path.isdir(path):
                 os.makedirs(path)
@@ -50,7 +104,7 @@ class P2NConfig:
 
     def getBoolean(self, line):
         s = self.getStr(line)
-        if s.count('True')>0 or s.count('true')>0:
+        if s.count('True') > 0 or s.count('true') > 0:
             return True # to gather contents
         else:
             return False
--- dex.js
+++ dex.js
@@ -1,6 +1,7 @@
 document.write('\
 <ul>\
 <li><a href="DATA/Lentille.html" target="_blank">Lentille</a></li>\
+<li><a href="DATA/Water.html" target="_blank">Water</a></li>\
 <li><a href="DATA/Respirator.html" target="_blank">Respirator</a></li>\
 </ul>\
 ');
